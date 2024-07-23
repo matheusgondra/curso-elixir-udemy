819ce0262a966b27723414e3d4b902e2cf111147
Matheus de Gondra <matheusgondra951@gmail.com>
2024-07-23 10:59:31 -0300

    corrigindo game over

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -19,11 +19,9 @@ defmodule ExMon do
   end
 
   def make_move(move) do
-    move
-    |> Actions.fetch_move()
-    |> do_move()
-
-    computer_move(Game.info())
+    Game.info()
+    |> Map.get(:status)
+    |> handle_status(move)
   end
 
   defp do_move({:error, move}), do: Status.print_wrong_move_message(move)
@@ -43,4 +41,14 @@ defmodule ExMon do
   end
 
   defp computer_move(_), do: :ok
+
+  defp handle_status(:game_over, _move), do: Status.print_round_message(Game.info())
+
+  defp handle_status(_status, move) do
+    move
+    |> Actions.fetch_move()
+    |> do_move()
+
+    computer_move(Game.info())
+  end
 end
